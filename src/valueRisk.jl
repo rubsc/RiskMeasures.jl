e5cddfc1950fad6209f48a8e27064d75d3ce02dc
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-07 09:01:19 +0100

    updated AVaR

--- src/valueRisk.jl
+++ src/valueRisk.jl
@@ -116,11 +116,7 @@ function AVaR(states,prob, alpha)
        tOpt = sqrt.(2 *alpha/var2)
        EV = Model(with_optimizer(Ipopt.Optimizer, print_level=0))
 		function objective1(t)
-			if t==0
-				return(10000000000)
-			else
-				return( t + 1/(1-alpha) * dot(prob, max.(states .- t,0)) )
-			end
+			return( t + 1/(1-alpha) * dot(prob, max.(states .- t,0)) )
 		end
 		register(EV, :objective1, 1, objective1, autodiff=true)
 	    @variable(EV,t,start = tOpt)
