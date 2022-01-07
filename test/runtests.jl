fbcfe4f99010dda6fda0c67a86f50ea95a89a1f7
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-07 09:00:18 +0100

    added test for esssup

--- test/runtests.jl
+++ test/runtests.jl
@@ -37,6 +37,7 @@ end
     @test EVaR(0.0,states, prob)[1] == Expectation(states,prob)
     @test EVaR(0.5,states, prob)[1] ≈ 3.413183 atol = 0.001
     @test EVaR(0.5,[1 1], [0.0 0.0])[1] == 0.0
+    @test EVaR(5.5,states, prob)[1] == maximum(states)
     @test AVaR(states,prob, 0.0)[1] == Expectation(states,prob)
     @test AVaR(states,prob, 0.5)[1] ≈ CTE2(states,prob,0.5) atol = 0.0001
 end
