2567b2e7efe9378d15d23c439f5fb6b3eba90e2b
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-24 21:30:52 +0100

    updated test to Float32

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,13 +25,13 @@ end
     @test Expectation([1 2 3 4],0) == sum([1 2 3 4])/4
     @test Expectation(states,prob) == 0.2*1 + 0.4*2 + 0.2*3 + 0.2*4
 
-    @test mSD(states,prob,0.0,2) == Expectation(states,prob)
-    @test mSD([1 1], [0.0 0.0],0.0,2) == 1
-    @test mSD(states,prob,1.0,2) ≈ 3.16419 atol = 0.001
-    @test mSD(states,prob,1.0,2) < mSD(states,prob,1.0,3)
+    @test mSD(states,prob,0.0f0,2.0f0) == Expectation(states,prob)
+    @test mSD([1 1], [0.0 0.0],0.0f0,2.0f0) == 1
+    @test mSD(states,prob,1.0f0,2.0f0) ≈ 3.16419 atol = 0.001
+    @test mSD(states,prob,1.0f0,2.0f0) < mSD(states,prob,1.0f0,3.0f0)
 
-    @test VaR(states,prob,0.0) == minimum(states)
-    @test CTE2(states,prob,0.0) == Expectation(states,prob)
+    @test VaR(states,prob,0.0f0) == minimum(states)
+    @test CTE(states,prob,0.0f0) == Expectation(states,prob)
 end
 
 
@@ -39,15 +39,15 @@ end
     # Write your tests here.
     states = [1 2 3 4]; prob = [0.2 0.4 0.2 0.2]
     
-    @test EVaR2(states, prob,0.0)[1] == Expectation(states,prob)
-    @test EVaR2(states, prob,0.5)[1] ≈ 3.413183 atol = 0.001
-    @test EVaR2([1 1], [0.0 0.0],0.5)[1] == 0 
-    @test EVaR(states, prob,0.0)[1] == Expectation(states,prob)
-    @test EVaR(states, prob,0.5)[1] ≈ 3.413183 atol = 0.001
-    @test EVaR([1 1], [0.0 0.0],0.5)[1] == 0.0
-    @test EVaR(states, prob,5.5)[1] == maximum(states)
-    @test AVaR(states,prob, 0.0)[1] == Expectation(states,prob)
-    @test AVaR(states,prob, 0.5)[1] ≈ CTE2(states,prob,0.5) atol = 0.0001
+    @test EVaR2(states, prob,0.0f0)[1] == Expectation(states,prob)
+    @test EVaR2(states, prob,0.5f0)[1] ≈ 3.413183 atol = 0.001
+    @test EVaR2([1 1], [0.0 0.0],0.5f0)[1] == 0 
+    @test EVaR(states, prob,0.0f0)[1] == Expectation(states,prob)
+    @test EVaR(states, prob,0.5f0)[1] ≈ 3.413183 atol = 0.001
+    @test EVaR([1 1], [0.0 0.0],0.5f0)[1] == 0.0
+    @test EVaR(states, prob,5.5f0)[1] == maximum(states)
+    @test AVaR(states,prob, 0.0f0)[1] == Expectation(states,prob)
+    @test AVaR(states,prob, 0.5f0)[1] ≈ CTE(states,prob,0.5f0) atol = 0.0001
 end
 
 
@@ -56,24 +56,24 @@ end
     states = [1 2 3 4]; prob = [0.2 0.4 0.2 0.2]
 
     
-    @test entropic(states, prob,1.0) ≈ 2.91430 atol = 0.001
-    @test entropic([1 1], [0.0 0.0],0.5) == 1.0
-    @test entropic([1 1], [0.0 0.0],0.0) === nothing
+    @test entropic(states, prob,1.0f0) ≈ 2.91430 atol = 0.001
+    @test entropic([1 1], [0.0 0.0],0.5f0) == 1.0
+    @test entropic([1 1], [0.0 0.0],0.0f0) === nothing
 
-    @test meanVariance([1 1], [0.0 0.0],-1.0) === nothing
-    @test meanVariance([1 1], [0.0 0.0],1.0) == 1.0
+    @test meanVariance([1 1], [0.0 0.0],-1.0f0) === nothing
+    @test meanVariance([1 1], [0.0 0.0],1.0f0) == 1.0
 
-    @test meanDeviation([1 1], [0.0 0.0],-1.0,2.0) === nothing
-    @test meanDeviation([1 1], [0.0 0.0],1.0,2.0) == 1.0
-    @test meanDeviation([1 1], [0.0 0.0],-1.0,0.5) === nothing
-    @test meanDeviation([1 1], [0.0 0.0],1.0,0.5) === nothing
+    @test meanDeviation([1 1], [0.0 0.0],-1.0f0,2.0f0) === nothing
+    @test meanDeviation([1 1], [0.0 0.0],1.0f0,2.0f0) == 1.0
+    @test meanDeviation([1 1], [0.0 0.0],-1.0f0,0.5f0) === nothing
+    @test meanDeviation([1 1], [0.0 0.0],1.0f0,0.5f0) === nothing
 
-    @test meanSemiVariance([1 1], [0 0],1.0, 0.0) == 2.0
-    @test meanSemiVariance([1 1], [0 0],-1.0, 0.0) === nothing
+    @test meanSemiVariance([1 1], [0 0],1.0f0, 0.0f0) == 2.0
+    @test meanSemiVariance([1 1], [0 0],-1.0f0, 0.0f0) === nothing
 
-    @test meanSemiDevi([1 1], [0 0],1.0, 0.0, 2.0) == 2.0
-    @test meanSemiDevi([1 1], [0 0],-1.0, 0.0, 2.0) === nothing
-    @test meanSemiDevi([1 1], [0 0],1.0, 0.0, -2.0) === nothing
+    @test meanSemiDevi([1 1], [0 0],1.0f0, 0.0f0, 2.0f0) == 2.0
+    @test meanSemiDevi([1 1], [0 0],-1.0f0, 0.0f0, 2.0f0) === nothing
+    @test meanSemiDevi([1 1], [0 0],1.0f0, 0.0f0, -2.0f0) === nothing
 
     @test spectral([0, 1], [0.2, 0.8], x -> 2.0*x) ≈ 0.96 atol = 0.0001
     @test spectral([0, 1], [0.2, 0.8], x -> x) === nothing
