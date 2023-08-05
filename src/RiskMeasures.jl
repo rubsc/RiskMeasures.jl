f826f2ad0956291929960b2706da029554133c06
Ruben Schlotter <ruben.schlotter@gmail.com>
2023-08-05 20:57:57 +0200

    initial upload

--- src/RiskMeasures.jl
+++ src/RiskMeasures.jl
@@ -1,5 +1,14 @@
 module RiskMeasures
 
-# Write your package code here.
+using JuMP, LinearAlgebra, Ipopt, Distributions, QuadGK, Test
 
+include("helper.jl")
+include("basic_RM.jl")
+include("valueRisk.jl")
+include("genRM.jl")
+
+export CTE, EVaR, EVaR2, Expectation, mSD, pnorm, ontoSimplex, goldenSearch, VaR, AVaR,
+        entropic, meanVariance, meanDeviation, meanSemiVariance, meanSemiDevi,spectral, GenCoherent,
+        GenConvex, checkSpectral, distortion, 
+        add_expr, math_expr
 end
